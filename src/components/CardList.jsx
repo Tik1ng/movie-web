ee1ceedbc846d0341d870caaf1f2e5482eef2632
Tugsbiligt <155721097+Tik1ng@users.noreply.github.com>
2025-05-15 14:00:09 +0800

    Update CardList.jsx

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,193 +1,26 @@
-import MovieCard from "./MovieCard";
-const data = [
-  {
-    adult: false,
-    imageUrl:
-      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1636996180",
-    backdrop_path: "/fTrQsdMS2MUw00RnzH0r3JWHhts.jpg",
-    genre_ids: [28, 80, 53],
-    id: 1197306,
-    original_language: "en",
-    original_title: "A Working Man",
-    overview:
-      "Levon Cade left behind a decorated military career in the black ops to live a simple life working construction. But when his boss's daughter, who is like family to him, is taken by human traffickers, his search to bring her home uncovers a world of corruption far greater than he ever could have imagined.",
-    popularity: 326.9183,
-    poster_path: "/6FRFIogh3zFnVWn7Z6zcYnIbRcX.jpg",
-    release_date: "2025-03-26",
-    title: "A Working Man",
-    video: false,
-    vote_average: 6.5,
-    vote_count: 613,
-  },
-  {
-    adult: false,
-    imageUrl:
-      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1636996180",
-    backdrop_path: "/2Nti3gYAX513wvhp8IiLL6ZDyOm.jpg",
-    genre_ids: [10751, 35, 12, 14],
-    id: 950387,
-    original_language: "en",
-    original_title: "A Minecraft Movie",
-    overview:
-      "Four misfits find themselves struggling with ordinary problems when they are suddenly pulled through a mysterious portal into the Overworld: a bizarre, cubic wonderland that thrives on imagination. To get back home, they'll have to master this world while embarking on a magical quest with an unexpected, expert crafter, Steve.",
-    popularity: 280.735,
-    poster_path: "/iPPTGh2OXuIv6d7cwuoPkw8govp.jpg",
-    release_date: "2025-03-31",
-    title: "A Minecraft Movie",
-    video: false,
-    vote_average: 6.163,
-    vote_count: 855,
-  },
-  {
-    adult: false,
-    imageUrl:
-      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1636996180",
-    backdrop_path: "/12tEzU0bNYKIjXXEwI5abuOotHF.jpg",
-    genre_ids: [37],
-    id: 710258,
-    original_language: "en",
-    original_title: "Rust",
-    overview:
-      'Infamous outlaw Harland Rust breaks his estranged grandson Lucas out of prison, after Lucas is convicted to hang for an accidental murder. The two must outrun legendary U.S Marshal Wood Helm and bounty hunter Fenton "Preacher" Lang who are hot on their tails. Deeply buried secrets rise from the ashes and an unexpected familial bond begins to form as the mismatched duo tries to survive the merciless American Frontier.',
-    popularity: 249.2081,
-    poster_path: "/tbJ3RkA2s6X5qrBzrYHYTxvDBui.jpg",
-    release_date: "2025-05-01",
-    title: "Rust",
-    video: false,
-    vote_average: 6.365,
-    vote_count: 37,
-  },
-  {
-    adult: false,
-    imageUrl:
-      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1636996180",
-    backdrop_path: "/dTGECn8He16tZeBHQTLf6rVydE8.jpg",
-    genre_ids: [80, 53, 18],
-    id: 302946,
-    original_language: "en",
-    original_title: "The Accountant",
-    overview:
-      "As a math savant uncooks the books for a new client, the Treasury Department closes in on his activities and the body count starts to rise.",
-    popularity: 178.8576,
-    poster_path: "/fceheXB5fC4WrLVuWJ6OZv9FXYr.jpg",
-    release_date: "2016-10-03",
-    title: "The Accountant",
-    video: false,
-    vote_average: 7.102,
-    vote_count: 6535,
-  },
-  {
-    adult: false,
-    imageUrl:
-      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1636996180",
-    backdrop_path: "/9yMmIou6xMtw2A61xiMsEcAhTrL.jpg",
-    genre_ids: [28, 18, 35],
-    id: 897160,
-    original_language: "ko",
-    original_title: "용감한 시민",
-    overview:
-      "An expelled boxing champion, who now is a high-school teacher, witnesses intolerable violence and throws her first punch to build justice against it, while putting on a mask.",
-    popularity: 259.3836,
-    poster_path: "/ixQoOExnnvIxYvnqGgfhaWqXeXc.jpg",
-    release_date: "2023-10-25",
-    title: "Brave Citizen",
-    video: false,
-    vote_average: 7.1,
-    vote_count: 35,
-  },
-  {
-    adult: false,
-    imageUrl:
-      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1636996180",
-    backdrop_path: "/8eifdha9GQeZAkexgtD45546XKx.jpg",
-    genre_ids: [28, 53, 878],
-    id: 822119,
-    original_language: "en",
-    original_title: "Captain America: Brave New World",
-    overview:
-      "After meeting with newly elected U.S. President Thaddeus Ross, Sam finds himself in the middle of an international incident. He must discover the reason behind a nefarious global plot before the true mastermind has the entire world seeing red.",
-    popularity: 223.1477,
-    poster_path: "/pzIddUEMWhWzfvLI3TwxUG2wGoi.jpg",
-    release_date: "2025-02-12",
-    title: "Captain America: Brave New World",
-    video: false,
-    vote_average: 6.1,
-    vote_count: 1813,
-  },
-  {
-    adult: false,
-    imageUrl:
-      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1636996180",
-    backdrop_path: "/65MVgDa6YjSdqzh7YOA04mYkioo.jpg",
-    genre_ids: [28, 80, 53],
-    id: 668489,
-    original_language: "en",
-    original_title: "Havoc",
-    overview:
-      "When a drug heist swerves lethally out of control, a jaded cop fights his way through a corrupt city's criminal underworld to save a politician's son.",
-    popularity: 179.5898,
-    poster_path: "/r46leE6PSzLR3pnVzaxx5Q30yUF.jpg",
-    release_date: "2025-04-24",
-    title: "Havoc",
-    video: false,
-    vote_average: 6.531,
-    vote_count: 580,
-  },
-  {
-    adult: false,
-    imageUrl:
-      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1636996180",
-    backdrop_path: "/mrmaTVp7PTBkjj5G62cE9pjrENg.jpg",
-    genre_ids: [27, 18],
-    id: 1359977,
-    original_language: "en",
-    original_title: "Conjuring the Cult",
-    overview:
-      "After discovering his blood-soaked daughter dead in the bathtub, David Bryson attends a self-help group to help save him from his ghostly nightmares. But when a group of mysterious cult-like women offer to help him resurrect his daughter. David's choices will not just decide his fate... but the fate of his dead daughter's SOUL.",
-    popularity: 239.7477,
-    poster_path: "/t4MiAeYpjL7saYvqvcn9xtOfA4K.jpg",
-    release_date: "2024-10-01",
-    title: "Conjuring the Cult",
-    video: false,
-    vote_average: 3.8,
-    vote_count: 12,
-  },
-  {
-    adult: false,
-    imageUrl:
-      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/movie-poster-template-design-21a1c803fe4ff4b858de24f5c91ec57f_screen.jpg?ts=1636996180",
-    backdrop_path: "/op3qmNhvwEvyT7UFyPbIfQmKriB.jpg",
-    genre_ids: [28, 14, 12],
-    id: 324544,
-    original_language: "en",
-    original_title: "In the Lost Lands",
-    overview:
-      "A queen sends the powerful and feared sorceress Gray Alys to the ghostly wilderness of the Lost Lands in search of a magical power, where she and her guide, the drifter Boyce, must outwit and outfight both man and demon.",
-    popularity: 181.2715,
-    poster_path: "/dDlfjR7gllmr8HTeN6rfrYhTdwX.jpg",
-    release_date: "2025-02-27",
-    title: "In the Lost Lands",
-    video: false,
-    vote_average: 6.324,
-    vote_count: 352,
-  },
-];
-
-const CardList = () => {
+const MovieCard = ({ image, rating, title }) => {
   return (
-    <div className="w-[1277px] grid grid-cols-5 grid-rows-2 gap-8">
-      {data.map((movie) => {
-        return (
-          <MovieCard
-            key={movie.id}
-            image={movie.imageUrl}
-            title={movie.original_title}
-            rating={movie.vote_average}
-          />
-        );
-      })}
+    <div className="w-full h-auto flex flex-col">
+      <img
+        src={image}
+        alt={`${title} poster`}
+        className="w-full aspect-[2/3] object-cover rounded-t-md"
+      />
+      <div className="flex flex-col p-2 bg-[#F4F4F5] rounded-b-md">
+        <div className="flex gap-0.5 items-center">
+          <img src="star.svg" alt="star icon" className="w-4 h-4" />
+          <p className="text-sm text-[#09090B] font-normal">
+            {rating}
+            <span className="text-[#71717A]">/10</span>
+          </p>
+        </div>
+        <p className="text-[#09090B] text-base sm:text-lg font-normal line-clamp-2">
+          {title}
+        </p>
+      </div>
     </div>
   );
 };
 
-export default CardList;
+export default MovieCard;
+
