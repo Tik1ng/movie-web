6841aec057abe42312b07fd86467c43fce94b150
Tugsbiligt <155721097+Tik1ng@users.noreply.github.com>
2025-05-15 14:02:48 +0800

    Update Upcoming.jsx

--- src/components/Upcoming.jsx
+++ src/components/Upcoming.jsx
@@ -1,15 +1,11 @@
 const Upcoming = () => {
   return (
-    <div>
-      <div className="flex justify-between text-[#09090B] w-[1277px] pb-8 ">
-        <p className="text-2xl font-semibold leading-8">Upcoming </p>
+    <div className="w-full max-w-7xl mx-auto px-4">
+      <div className="flex justify-between text-[#09090B] pb-4 md:pb-8">
+        <p className="text-xl md:text-2xl font-semibold leading-8">Upcoming</p>
         <div className="flex py-2 px-4 gap-2 items-center">
           <p className="text-sm font-medium leading-5">See more</p>
-          <img
-            src="arrow-right.svg"
-            alt="arrow icon"
-            className="w-[16px] h-[16px]"
-          />
+          <img src="arrow-right.svg" alt="arrow icon" className="w-4 h-4" />
         </div>
       </div>
     </div>
