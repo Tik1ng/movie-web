655ba208f4ba0db5aed22e6fb45b35e1f8bbbc10
Tugsbiligt <155721097+Tik1ng@users.noreply.github.com>
2025-05-15 14:00:39 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,31 @@
 const Hero = () => {
   return (
     <div
-      className="w-[1440px] h-[600px] bg-cover bg-center my-7 "
+      className="w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] bg-cover bg-center my-4 md:my-7"
       style={{ backgroundImage: "url('Feature.png')" }}
     >
-      <div className="flex flex-col gap-3 w-[404px] text-white pl-44 pt-48">
-        <div>
-          <p className="text-base font-normal leading-6">Now Playing:</p>
-          <p className="text-4xl font-bold leading-10">Wicked</p>
-          <div className="flex">
-            <img src="star.svg" alt="stat icon" />
-            <p className="text-lg font-semibold leading-7">
-              6.9 <span className=" text-[#71717A]">/10</span>
-            </p>
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col gap-3 max-w-sm text-white pt-16 sm:pt-24 md:pt-32 lg:pt-48">
+          <div>
+            <p className="text-base font-normal leading-6">Now Playing:</p>
+            <p className="text-3xl md:text-4xl font-bold leading-10">Wicked</p>
+            <div className="flex items-center">
+              <img src="star.svg" alt="star icon" className="mr-1" />
+              <p className="text-lg font-semibold leading-7">
+                6.9 <span className="text-[#71717A]">/10</span>
+              </p>
+            </div>
           </div>
-        </div>
-        <p className="w-[302px] text-xs font-normal leading-4w">
-          Elphaba, a misunderstood young woman because of her green skin, and
-          Glinda, a popular girl, become friends at Shiz University in the Land
-          of Oz. After an encounter with the Wonderful Wizard of Oz, their
-          friendship reaches a crossroads.{" "}
-        </p>
-        <div className="w-[145px] flex px-4 py-2 gap-2 bg-white rounded-md">
-          <img src="play.svg" alt="play icon" />
-          <p className="text-sm font-medium  text-[#18181B]">Watch Trailer</p>
+          <p className="text-xs md:text-sm font-normal leading-normal max-w-xs md:max-w-sm">
+            Elphaba, a misunderstood young woman because of her green skin, and
+            Glinda, a popular girl, become friends at Shiz University in the
+            Land of Oz. After an encounter with the Wonderful Wizard of Oz,
+            their friendship reaches a crossroads.
+          </p>
+          <div className="inline-flex w-40 px-4 py-2 gap-2 bg-white rounded-md items-center">
+            <img src="play.svg" alt="play icon" />
+            <p className="text-sm font-medium text-[#18181B]">Watch Trailer</p>
+          </div>
         </div>
       </div>
     </div>
@@ -31,3 +33,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
